d30324d9e5537a1778e5d474dfd8dbed8277f743
shahir099 <36823171+shahir099@users.noreply.github.com>
2018-09-07 23:50:18 +0600

    First Commit of BookYourMeal Server part

---
+++ app/src/main/java/com/example/shahir/bookyourmealserver/Common/Common.java
@@ -0,0 +1,7 @@
+package com.example.shahir.bookyourmealserver.Common;
+
+import com.example.shahir.bookyourmealserver.Model.User;
+
+public class Common {
+    public static User currentUser;
+}
---
+++ app/src/main/java/com/example/shahir/bookyourmealserver/Interface/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.shahir.bookyourmealserver.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view,int position,boolean isLongClick);
+}
